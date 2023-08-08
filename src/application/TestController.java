dde83a47d463958e89fab507e20c6387b5e89b82
Patriciaa <patricia.c2005@aluno.ifsc.edu.br>
2023-08-08 04:13:37 -0300

    delete arquivo DAO vendedor

--- src/application/TestController.java
+++ src/application/TestController.java
@@ -3,7 +3,7 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import controle.VendedorDAO;
+import controle.VendedoresDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
--- src/controle/VendedoresDAO.java
+++ src/controle/VendedoresDAO.java
@@ -2,36 +2,91 @@ package controle;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import modelo.EnumRoles;
 import modelo.Vendedores;
 
 public class VendedoresDAO {
 
+	public ArrayList<Vendedores> listar() {
+	    Conexao c = Conexao.getInstancia();
+	    Connection con = c.conectar();
+	    
+	    ArrayList<Vendedores> vendedores = new ArrayList<>();
+	    
+	    String query = "SELECT * FROM vendedores";
+	    try {
+	        PreparedStatement ps = con.prepareStatement(query);
+	        ResultSet rs = ps.executeQuery();
+	        
+	        while (rs.next()) {
+	            int idVendedor = rs.getInt("idvendedores");
+	            String nome = rs.getString("nome");
+	            double salario = rs.getDouble("salario");
+	            
+	            // Recupere o valor do cargo do enum
+	            String cargoString = rs.getString("cargo");
+	            EnumRoles cargo = EnumRoles.valueOf(cargoString);
+	            
+	            Vendedores v = new Vendedores();
+	            v.setIdVendedores(idVendedor);
+	            v.setNome(nome);
+	            v.setSalario(salario);
+	            v.setCargo(cargo);
+	            
+	            vendedores.add(v);
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        c.fecharConexao();
+	    }
+	    
+	    return vendedores;
+	}
+
+	
 	public boolean inserir(Vendedores v) {
 
 		Conexao c = Conexao.getInstancia();
+
 		Connection con = c.conectar();
-		String query = "INSERT INTO (idVendedores, salario, nome, sobrenome) VALUES (?, ?, ?, ?);";
 
-		try {
-			PreparedStatement ps = con.prepareStatement(query);
-			ps.setString(1, Integer.toString(v.getIdVendedores()));
-			ps.setString(2, Double.toString(v.getSalario()));
-			ps.setString(2, v.getNome());
-			ps.setString(4, v.getSobrenome());
+		String query = 
+				"INSERT INTO vendedores "
+				+ "(idvendedores,"
+				+ " salario, "
+				+ "nome,"
+				+ " cargo)"
+				+ " VALUES (?, ?, ?, ?, ?)";
+		
+		
+		 try {
+	            PreparedStatement ps = con.prepareStatement(query);
 
-			ps.executeUpdate();
+	            ps.setInt(1, v.getIdVendedores());
+	            ps.setDouble(2, v.getSalario());
+	            ps.setString(3, v.getNome());
 
-			c.fecharConexao();
+	            // Defina o cargo diretamente usando o valor do enum
+	            ps.setString(5, EnumRoles.FUNCIONARIO.name());
+	            
+	            
+	            // Consolidar a execução do comando no banco
+	            ps.executeUpdate();
 
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			return false;
-		}
+	            // Fecha a conexão com o banco
+	            c.fecharConexao();
+	            
+	        } catch (SQLException e1) {
+	            // TODO Auto-generated catch block
+	            e1.printStackTrace();
+	            return false; // Retorna false em caso de falha na inserção
+	        }
 
-		return true;
-
-	}
-
+	        return true; // Retorna true se a inserção for bem-sucedida
+	    }
 }
