a5387e90f5223cf8cbbce800168273082d5a4ba2
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-08-15 20:05:22 -0300

    tentativa de fazer p controller funcionar deus tenha piedade dessa jovem - andri

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,26 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
-
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/visao/testeAndri.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/visao/Cadastro_funcionario.fxml"));
+        Parent root = loader.load();
+
+        TestController controller = loader.getController(); // Obtém uma referência ao controlador
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
--- src/application/TestController.java
+++ src/application/TestController.java
@@ -1,5 +1,55 @@
 package application;
 
-public class TestController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TestController implements Initializable{
+	
+	 @FXML
+	    private Button btnCadastro;
+
+	    @FXML
+	    private TextField txtCpf;
+
+	    @FXML
+	    private TextField txtEmailF;
+
+	    @FXML
+	    private TextField txtNomeF;
+
+	    @FXML
+	    private PasswordField txtSenhaF;
+	    @FXML
+	    private Label lblCpf;
+
+	    @FXML
+	    private Label lblEmail;
+
+	    @FXML
+	    private Label lblNome;
+
+	    @FXML
+	    private Label lblSenha;
+
+
+	    @FXML
+	   private void cadastrarFuncionario(ActionEvent event) {
+	    	System.out.println("você clicou ...");
+	    	lblNome.setText("ola mundo "+ txtNomeF.getText());
+	    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
