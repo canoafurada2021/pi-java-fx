a4e92e591642a0057684afd775c4622e1b1a8692
Patriciaa <patricia.c2005@aluno.ifsc.edu.br>
2023-08-19 11:55:49 -0300

    Documentação base de como realizar mais de um INNER JOIN

--- src/controle/FornecedorDAO.java
+++ src/controle/FornecedorDAO.java
@@ -11,60 +11,68 @@ import modelo.Fornecedores;
 
 public class FornecedorDAO {
 
-	public ArrayList<Fornecedores> listar(){
-	    Conexao c = Conexao.getInstancia();
-	    Connection con = c.conectar();
-	    
-	    ArrayList<Fornecedores> fornecedores = new ArrayList<>();
-	    
-	    
-	    String query = "SELECT f.*, e.rua FROM fornecedores f " +
-                "INNER JOIN enderecos e ON f.enderecos_id = e.id";	    
-	    
-	    
-	    try {
-	        PreparedStatement ps = con.prepareStatement(query);
-	        ResultSet rs = ps.executeQuery();
-	    	
-	        
-	        
-	    	while(rs.next()) {
-	    		
-	    		
-	    	int cnpj = rs.getInt("cnpj");
-	    	String nome = rs.getString("nome");
-	    	Long telefone = rs.getLong("telefone");
-	    	String atividades = rs.getString("atividades");
-	    	
-	    	Fornecedores f = new Fornecedores();
-	    	
-	    	f.setCnpj(cnpj);
-	    	f.setNome(nome);
-	    	f.setTelefone(telefone);
-	    	f.setAtividades(atividades);
-	    	
-	    	  // Crie um objeto Endereco e configure os atributos
-            Endereco endereco = new Endereco();
-            endereco.setRua(rs.getString("rua"));
-            // Preencha os demais atributos do endereço se necessário
+	public ArrayList<Fornecedores> listar() {
+		Conexao c = Conexao.getInstancia();
+		Connection con = c.conectar();
 
-            // Associe o endereço ao fornecedor
-            f.setEnderecoId(endereco);
+		ArrayList<Fornecedores> fornecedores = new ArrayList<>();
+		// Caso a tabela a ser trabalhada no método de listagem houver uma chave
+		// estrangeira vinculada a outra tabela
+		// Será necessário aplicar um inner join (junção entre duas ou mais tabelas)
+		// para ter o retorno correto dos dados
+		// o exemplo abaixo faz uso do inner join na tabela fornecedores que possui a
+		// chave estrangeira para endereços
 
-            fornecedores.add(f);
-            
-            
-	    	
-	    	}
-	    }catch (SQLException e) {
-	        e.printStackTrace();
-	    } finally {
-	        c.fecharConexao();
-	    }
-	    return fornecedores;
-	    
+		// Exemplo de como fazer mais de um inner join (apenas didático, essas tabelas
+		// não existem no projeto):
+
+		// onde: 'c' se refere a 'clientes', 'p' a 'pedidos' e 'ip' a 'itens_pedidos'
+
+		// SELECT c.nome, p.data_pedido, ip.produto, ip.quantidade
+		// FROM clientes c
+		// INNER JOIN pedidos p ON c.id_cliente = p.id_cliente
+		// INNER JOIN itens_pedido ip ON p.id_pedido = ip.id_pedido;
+
+		String query = "SELECT f.*, e.rua FROM fornecedores f " + "INNER JOIN enderecos e ON f.enderecos_id = e.id";
+
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+				int cnpj = rs.getInt("cnpj");
+				String nome = rs.getString("nome");
+				Long telefone = rs.getLong("telefone");
+				String atividades = rs.getString("atividades");
+
+				Fornecedores f = new Fornecedores();
+
+				f.setCnpj(cnpj);
+				f.setNome(nome);
+				f.setTelefone(telefone);
+				f.setAtividades(atividades);
+
+				// Crie um objeto Endereco e configure os atributos
+				Endereco endereco = new Endereco();
+				endereco.setRua(rs.getString("rua"));
+				// Preencha os demais atributos do endereço se necessário
+
+				// Associe o endereço ao fornecedor
+				f.setEnderecoId(endereco);
+
+				fornecedores.add(f);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			c.fecharConexao();
+		}
+		return fornecedores;
+
 	}
-	
+
 	public boolean inserir(Fornecedores f) {
 
 		// Instanciar a classe
@@ -73,13 +81,8 @@ public class FornecedorDAO {
 		// Abrir a conexao com o banco de dados
 		Connection con = c.conectar();
 
-		String query = "INSERT INTO fornecedores "
-				+ "(" + "cnpj,"
-				+ "enderecos_id,"
-				+ "nome," 
-				+ "atividades,"
-				+ "telefone) "
-				+ "VALUES (?, ?, ?, ?);";
+		String query = "INSERT INTO fornecedores " + "(" + "cnpj," + "enderecos_id," + "nome," + "atividades,"
+				+ "telefone) " + "VALUES (?, ?, ?, ?);";
 
 		try {
 			PreparedStatement ps = con.prepareStatement(query);
