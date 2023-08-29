830c4d01db348701db9d0cc2ca5915e0da762c34
Emily Joanna Alves <emily.a24@aluno.ifsc.edu.br>
2023-08-29 12:00:03 -0300

    Fazendo inserts clientes

--- src/resources/scriptFinal.sql
+++ src/resources/scriptFinal.sql
@@ -22,6 +22,7 @@ CREATE TABLE IF NOT EXISTS `login` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empresa` (
   `id_empresa` INT NOT NULL AUTO_INCREMENT,
+  `img_Base64Empresa` VARCHAR(555) NOT NULL DEFAULT "",
   `razao_social` VARCHAR(100) NOT NULL,
   `telefone` VARCHAR(20) NOT NULL,
   `cnpj` VARCHAR(18) NOT NULL,
@@ -41,6 +42,7 @@ CREATE TABLE IF NOT EXISTS `empresa` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `vendedor` (
   `id_vendedor` INT NOT NULL,
+  `img_Base64Vendedor` VARCHAR(555) NOT NULL DEFAULT "",
   `salario` DOUBLE NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `cargo` ENUM('MASTER', 'FUNCIONARIO', 'CLIENTE') NOT NULL, -- Coluna para o cargo diretamente
@@ -66,6 +68,7 @@ CREATE TABLE IF NOT EXISTS `enderecos` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `locador` (
   `pessoas_cpf` INT NOT NULL,
+
   `tel_contato` LONG NOT NULL,
   `pais_residencia` VARCHAR(45) NOT NULL,
   `cnh` LONG NOT NULL,
@@ -73,11 +76,10 @@ CREATE TABLE IF NOT EXISTS `locador` (
   `num_identificacao_carteira` LONG NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `sobrenome` VARCHAR(45) NOT NULL,
-  `cargo` ENUM('MASTER', 'FUNCIONARIO', 'CLIENTE') NOT NULL, -- Coluna para o cargo diretamente
+  `cargo` ENUM('MASTER', 'FUNCIONARIO', 'CLIENTE') NOT NULL, -- Coluna para o cargo diretamente  
+  `img_Base64Locador` VARCHAR(555) NOT NULL DEFAULT "",
   PRIMARY KEY (`pessoas_cpf`)
 );
-
-insert into locador (pessoas_cpf, tel_contato, pais_residencia,cnh,validade_carteira,  num_identificacao_carteira, nome, sobrenome, cargo) values ('212213454', 47988989,'BRASIL', 89111042,'2023-08-20', 012344,'Emily Joanna',"Alves",   "CLIENTE");
 
 -- -----------------------------------------------------
 -- Table .`vendaRegistros`
@@ -153,24 +155,20 @@ CREATE TABLE IF NOT EXISTS `veiculo`(
 insert into empresa (razao_social, telefone, cnpj, nome_fantasia, porte_empresa, cargo) values ('O objetivo da empresa é deixar as pessoas felizes andando de gol', '+55 47 992178827', '12.345.678/0001-99', 'Pope Francis Master', 'grande', "MASTER");
 -- ------------------------------------------------------------------------------------------------------
 
+-- + inserts de clientes p ficarem precadastrados --
+insert into locador (pessoas_cpf, tel_contato, pais_residencia,cnh,validade_carteira,  num_identificacao_carteira, nome, sobrenome, cargo, img_Base64Locador) values ('212213454', 47988989827, 'BRASIL', 89111042, '2023-08-20', 012344, 'Carl', 'Johnson', "CLIENTE", 'https://static.wikia.nocookie.net/gta/images/d/d4/CJ-GTASA-230px.png/revision/latest?cb=20141212032649&path-prefix=pt');
+insert into locador (pessoas_cpf, tel_contato, pais_residencia,cnh,validade_carteira,  num_identificacao_carteira, nome, sobrenome, cargo, img_Base64Locador) values ('123456789', 47992117973, 'BRASIL', 32165498, '2025-08-30', 987654, 'Frank', 'Tenpenny', "CLIENTE", 'https://static.wikia.nocookie.net/gta/images/0/02/GTASA_Tenpenny.jpg/revision/latest?cb=20090630163333&path-prefix=pt');
+-- FAZ MAIS PORRA - insert into locador (pessoas_cpf, tel_contato, pais_residencia,cnh,validade_carteira,  num_identificacao_carteira, nome, sobrenome, cargo, img_Base64Locador) values ('', , 'BRASIL', , '--', , '', '', "CLIENTE", '');
+
+-- inserts VENDEDORES	
+INSERT INTO vendedor (id_vendedor, salario, nome,  cargo) VALUES (1, 2574.89, 'Patricia Cordeiro', 'FUNCIONARIO');
+INSERT INTO vendedor (id_vendedor, salario, nome,  cargo) VALUES (3, 3452.90, 'Andrieli Mendes', 'FUNCIONARIO');
+
+-- inserts ENDERECOS
 insert into enderecos (cep, id, rua, bairro, cidade, estado) values ('89111042', '1', 'Rua Florianópolis', 'Bela Vista', 'Gaspar', 'Santa Catarina');
 insert into enderecos (cep, id, rua, bairro, cidade, estado) values ('891110826', '2', 'Rua João José Schmitz', 'Bela Vista', 'Gaspar', 'Santa Catarina');
 insert into enderecos (cep, id, rua, bairro, cidade, estado) values ('89025420', '3', 'Rua Juiz de Fora', 'Glória', 'Blumenau', 'Santa Catarina');
 insert into enderecos (cep, id, rua, bairro, cidade, estado) values ('88420000', '4', 'Estrada Geral Pitangueira', 'Estrada Geral', 'Agrolândia', 'Santa Catarina');
-
-
-
-
-
-INSERT INTO vendedor (id_vendedor, salario, nome,  cargo) VALUES (1, 2574.89, 'Patricia Cordeiro', 'FUNCIONARIO');
-INSERT INTO vendedor (id_vendedor, salario, nome,  cargo) VALUES (3, 3452.90, 'Andrieli Mendes', 'FUNCIONARIO');
-
-
-
-
-
-
--- inserts ENDERECOS
 
 -- inserts CATEGORIAS
 insert into categoria (id_categoria, categoria) values ('1', 'felis');
@@ -183,10 +181,8 @@ insert into categoria (id_categoria, categoria) values ('25', 'semper');
 insert into fornecedores (cnpj,  enderecos_id, nome, atividades, telefone) values (82, 1, 'Emily Joanna Alves', 'HB20 all black', 47984273688); 
 insert into fornecedores (cnpj,  enderecos_id, nome, atividades, telefone) values (83, 2, 'Maria Eduarda Mendes','Gol Bolinha 2014',  47997212305); 
 
-
-
 -- inserts PRODUTOS
--- TESTE DE PRODUTO PRE CADASTRADO -------------------------------------------------
+-- TESTE DE PRODUTO PRE CADASTRADO ------------------------ FAZER MAIS -------------------------
 insert into veiculo (id_veiculo, quant_assento, tipo_cambio, quant_portas, espaco_porta_malas, marca, nome, cor, ano, nota_avaliacao, preco_por_dia, img_base64, unidade_em_estoque, categoria_id_categoria, fornecedores_cnpj) values (1, 5, 'Automático', 4, '4', 'Volkswagem', 'Gol', 'Vermelho', '2018', 5.0, 300, 'https://quatrorodas.abril.com.br/wp-content/uploads/2021/02/volkswagen_gol_5-door_25.jpeg?quality=70&strip=info', 5, 23, 82);
 
 -- inserts ALUGUELREGISTROS
@@ -199,6 +195,9 @@ insert into login (id, cpf, senha) values (1, '13093824923', '24102005' ), (2, '
 
 -- --- CRIAR INSERTS PARA CRIACAO DE VEICULO NO SISTEMA 
 
+
+-- SELECTSSSSSSSSSSSSSSSSSS -----------------------------------------------------------------------------------------
+-- -------------------------------
 SELECT * FROM login;
 SELECT * FROM categoria;
 SELECT * FROM aluguelRegistros;
@@ -207,9 +206,3 @@ SELECT * FROM fornecedores;
 SELECT * FROM enderecos;
 SELECT * FROM empresa;
 SELECT * FROM locador;
--- SELECTSSSSSSSSSSSSSSSSSS -----------------------------------------------------------------------------------------
--- -------------------------------
-
-
--- SELECT P LISTAGEM
-
