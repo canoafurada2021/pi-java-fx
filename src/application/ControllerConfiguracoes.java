a3a359a6fd75d278879328b1e9a37ce57bd99e22
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-09-05 11:15:27 -0300

    correcoes telas e controllers, conversao correta do combo-box no cadastro de funcionarios, porém ainda nao está funcionando 100% por conta da role que falta fazer

---
+++ src/application/ControllerConfiguracoes.java
@@ -0,0 +1,155 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+public class ControllerConfiguracoes implements Initializable {
+
+	   @FXML
+	    private SplitPane SlipPaneConfigurações;
+
+	    @FXML
+	    private StackPane StackPanePerfil;
+
+	    @FXML
+	    private Button bntSalvar;
+
+	    @FXML
+	    private Button btnAlterar;
+
+	    @FXML
+	    private Button btnConfiguracoes;
+
+	    @FXML
+	    private Button btnDashboard;
+
+	    @FXML
+	    private Button btnFornecedores;
+
+	    @FXML
+	    private Button btnFuncionarios;
+
+	    @FXML
+	    private Button btnLocacao;
+
+	    @FXML
+	    private Button btnPerfil;
+
+	    @FXML
+	    private Button btnProdutos;
+
+	    @FXML
+	    private Button btnSair;
+
+	    @FXML
+	    private Button btnUsuarios;
+
+	    @FXML
+	    private ImageView imgConfiguracoes;
+
+	    @FXML
+	    private ImageView imgDefaultConfiguracoes;
+
+	    @FXML
+	    private ImageView imgDefaultDashboard;
+
+	    @FXML
+	    private ImageView imgDefaultFornecedores;
+
+	    @FXML
+	    private ImageView imgDefaultFuncionarios;
+
+	    @FXML
+	    private ImageView imgDefaultLocacao;
+
+	    @FXML
+	    private ImageView imgDefaultProdutos;
+
+	    @FXML
+	    private ImageView imgDefaultUsuarios;
+
+	    @FXML
+	    private ImageView imgFotoPerfil;
+
+	    @FXML
+	    private ImageView imgLogo;
+
+	    @FXML
+	    private ImageView imgSair;
+
+	    @FXML
+	    private Label lblCnpj;
+
+	    @FXML
+	    private Label lblDados;
+
+	    @FXML
+	    private Label lblFuncao;
+
+	    @FXML
+	    private Label lblId;
+
+	    @FXML
+	    private Label lblNomeFantasia;
+
+	    @FXML
+	    private Label lblNomeVendedor;
+
+	    @FXML
+	    private Label lblPorte;
+
+	    @FXML
+	    private Label lblRazaoSocial;
+
+	    @FXML
+	    private Label lblTelefone;
+
+	    @FXML
+	    private Pane panelConfiguracoes;
+
+	    @FXML
+	    private TextField txtCnpj;
+
+	    @FXML
+	    private TextField txtId;
+
+	    @FXML
+	    private TextField txtNomeFantasia;
+
+	    @FXML
+	    private TextField txtPorteEmpresa;
+
+	    @FXML
+	    private TextField txtRazaoSocial;
+
+	    @FXML
+	    private TextField txtTelefone;
+
+	    @FXML
+	    void alterarDados(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void salvarDados(ActionEvent event) {
+
+	    }
+
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+
+			
+		}
+
+}
--- src/application/ControllerTableViewFuncionarios.java
+++ src/application/ControllerTableViewFuncionarios.java
@@ -22,68 +22,62 @@ import modelo.Vendedor;
 
 public class ControllerTableViewFuncionarios implements Initializable {
 
-    @FXML
-    private TableView<Vendedor> tableFuncionario;
+	@FXML
+	private TableView<Vendedor> tableFuncionario;
 
-    @FXML
-    private TableColumn<Vendedor, String> columnCargo;
+	@FXML
+	private TableColumn<Vendedor, String> columnCargo;
 
-    @FXML
-    private TableColumn<Vendedor, Integer> columnIdVendedor;
+	@FXML
+	private TableColumn<Vendedor, Integer> columnIdVendedor;
 
-    @FXML
-    private TableColumn<Vendedor, String> columnNome;
+	@FXML
+	private TableColumn<Vendedor, String> columnNome;
 
-    @FXML
-    private TableColumn<Vendedor, Double> columnSalario;
+	@FXML
+	private TableColumn<Vendedor, Double> columnSalario;
 
-    private ObservableList<Vendedor> obsVendedores;
-    
-    @FXML
-    private void sair(ActionEvent event) {
-        // Implemente a lógica para sair aqui
-        System.out.println("Botão de sair pressionado");
-    }
+	private ObservableList<Vendedor> obsVendedores;
 
-    
-    @FXML
-    private void salvarDados(ActionEvent event) {
-    	System.out.println("AAAAAAAAAAAAAAAA");
-    }
-    
+	@FXML
+	private void sair(ActionEvent event) {
+		// Implemente a lógica para sair aqui
+		System.out.println("Botão de sair pressionado");
+	}
 
-    // Formata a cédula do salário para o padrão PT-BR (R$ 0000,000)
-    NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+	@FXML
+	private void salvarDados(ActionEvent event) {
+		System.out.println("AAAAAAAAAAAAAAAA");
+	}
 
-    
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-       
+	// Formata a cédula do salário para o padrão PT-BR (R$ 0000,000)
+	NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 
- columnIdVendedor.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getId_vendedor()));
-        columnNome.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNome()));
-        columnCargo.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getCargo().toString()));
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 
+		columnIdVendedor.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getId_vendedor()));
+		columnNome.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNome()));
+		columnCargo.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getCargo().toString()));
 
-        // Configura a formatação da célula da coluna de salário
-        columnSalario.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getSalario()));
+		// Configura a formatação da célula da coluna de salário
+		columnSalario.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getSalario()));
 
-        
-        
-        // Usa um StringConverter (Classe do FXML responsável por facilitar na conversão de tipos Long e Double pra String)
-        //para formatar o valor do salário como moeda
-        
-        StringConverter<Double> currencyConverter = new StringConverter<>() {
-            private final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		// Usa um StringConverter (Classe do FXML responsável por facilitar na conversão
+		// de tipos Long e Double pra String)
+		// para formatar o valor do salário como moeda
 
-            @Override
-            public String toString(Double value) {
-                if (value == null) {
-                    return "";
-                }
-                
-                return currencyFormat.format(value);
-            }
+		StringConverter<Double> currencyConverter = new StringConverter<>() {
+			private final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+
+			@Override
+			public String toString(Double value) {
+				if (value == null) {
+					return "";
+				}
+
+				return currencyFormat.format(value);
+			}
 
 			@Override
 			public Double fromString(String string) {
@@ -91,35 +85,34 @@ public class ControllerTableViewFuncionarios implements Initializable {
 				return null;
 			}
 
-        };
+		};
 
-        // Definição e criação das células na coluna 
-        //Expressão lambda para simplificar a criação de células
-        columnSalario.setCellFactory(tc -> new TableCell<Vendedor, Double>() {
-            @Override
-            protected void updateItem(Double salario, boolean empty) {
-                super.updateItem(salario, empty);
-                //Validação caso a coluna de salário esteja vazia, a cédula é setada como nullo ou " "
-                if (empty || salario == null) {
-                	
-                    setText("");
-                } else {
-                	//Trecho do código que faz a formatação do valor dentro da cédula
-                    setText(currencyConverter.toString(salario));
-                }
-            }
-        });
+		// Definição e criação das células na coluna
+		// Expressão lambda para simplificar a criação de células
+		columnSalario.setCellFactory(tc -> new TableCell<Vendedor, Double>() {
+			@Override
+			protected void updateItem(Double salario, boolean empty) {
+				super.updateItem(salario, empty);
+				// Validação caso a coluna de salário esteja vazia, a cédula é setada como nullo
+				// ou " "
+				if (empty || salario == null) {
 
-        carregarVendedores();
-    }
+					setText("");
+				} else {
+					// Trecho do código que faz a formatação do valor dentro da cédula
+					setText(currencyConverter.toString(salario));
+				}
+			}
+		});
 
+		carregarVendedores();
+	}
 
+	public void carregarVendedores() {
+		VendedorDAO dao = new VendedorDAO();
+		ArrayList<Vendedor> vendedores = dao.listar();
 
-    public void carregarVendedores() {
-        VendedorDAO dao = new VendedorDAO();
-        ArrayList<Vendedor> vendedores = dao.listar();
-        
-        obsVendedores = FXCollections.observableArrayList(vendedores);
-        tableFuncionario.setItems(obsVendedores);
-    }
+		obsVendedores = FXCollections.observableArrayList(vendedores);
+		tableFuncionario.setItems(obsVendedores);
+	}
 }
