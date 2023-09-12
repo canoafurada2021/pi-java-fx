b1256c368f717d0fb9a278e5dac0d01a4a6c4540
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-09-12 11:11:49 -0300

    Atualização Controller List Clientes

--- src/application/ControllerListClientes.java
+++ src/application/ControllerListClientes.java
@@ -1,5 +1,137 @@
 package application;
 
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
 public class ControllerListClientes {
+
+	@FXML
+	private SplitPane SlipPaneConfigurações;
+
+	@FXML
+	private StackPane StackPanePerfil;
+
+	@FXML
+	private Button bntSalvar;
+
+	@FXML
+	private Button btnConfiguracoes;
+
+	@FXML
+	private Button btnDashboard;
+
+	@FXML
+	private Button btnFornecedores;
+
+	@FXML
+	private Button btnFuncionarios;
+
+	@FXML
+	private Button btnLocacao;
+
+	@FXML
+	private Button btnPerfil;
+
+	@FXML
+	private ImageView btnPesquisa;
+
+	@FXML
+	private Button btnPesquisar;
+
+	@FXML
+	private Button btnProdutos;
+
+	@FXML
+	private Button btnSair;
+
+	@FXML
+	private Button btnUsuarios;
+
+	@FXML
+	private TableColumn<?, ?> columnAcoes;
+
+	@FXML
+	private TableColumn<?, ?> columnCNH;
+
+	@FXML
+	private TableColumn<?, ?> columnCPF;
+
+	@FXML
+	private TableColumn<?, ?> columnIdCarteira;
+
+	@FXML
+	private TableColumn<?, ?> columnNome;
+
+	@FXML
+	private TableColumn<?, ?> columnPaisResidencia;
+
+	@FXML
+	private TableColumn<?, ?> columnTelefone;
+
+	@FXML
+	private TableColumn<?, ?> columnValidadeCarteira;
+
+	@FXML
+	private ImageView imgDefaultConfiguracoes;
+
+	@FXML
+	private ImageView imgDefaultDashboard;
+
+	@FXML
+	private ImageView imgDefaultFornecedores;
+
+	@FXML
+	private ImageView imgDefaultFuncionarios;
+
+	@FXML
+	private ImageView imgDefaultLocacao;
+
+	@FXML
+	private ImageView imgDefaultProdutos;
+
+	@FXML
+	private ImageView imgDefaultUsuarios;
+
+	@FXML
+	private ImageView imgFotoPerfil;
+
+	@FXML
+	private ImageView imgLogo;
+
+	@FXML
+	private ImageView imgSair;
+
+	@FXML
+	private Label lblClientes;
+
+	@FXML
+	private Label lblFuncao;
+
+	@FXML
+	private Label lblNomeVendedor;
+
+	@FXML
+	private Pane panelConfiguracoes;
+
+	@FXML
+	private TableView<?> tableClientes;
+
+	@FXML
+	private TextField txtPesquisa;
+
+	@FXML
+	void salvarDados(ActionEvent event) {
+
+	}
 
 }
