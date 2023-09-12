b5e7aa327ea1f34a5b9672e93ad35c88b9412e62
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-09-12 10:59:00 -0300

    Controller da Tela de edição e atualização Tela
    A atualização foi em prol da mudança de alguns ids que estavam errados, podendo gerar conflito

---
+++ src/application/ControllerEdicaoFornecedores.java
@@ -0,0 +1,149 @@
+package application;
+
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+public class ControllerEdicaoFornecedores {
+
+	@FXML
+	private StackPane StackPanePerfil;
+
+	@FXML
+	private Button btnCancelar;
+
+	@FXML
+	private Button btnConfiguracoes;
+
+	@FXML
+	private Button btnDashboard;
+
+	@FXML
+	private Button btnFornecedores;
+
+	@FXML
+	private Button btnFuncionarios;
+
+	@FXML
+	private Button btnLocacao;
+
+	@FXML
+	private Button btnPerfil;
+
+	@FXML
+	private Button btnProdutos;
+
+	@FXML
+	private Button btnSair;
+
+	@FXML
+	private Button btnSalvar;
+
+	@FXML
+	private Button btnUsuarios;
+
+	@FXML
+	private ComboBox<?> comboEnderecoIds;
+
+	@FXML
+	private ImageView imgDefaultConfiguracoes;
+
+	@FXML
+	private ImageView imgDefaultDashboard;
+
+	@FXML
+	private ImageView imgDefaultFornecedores;
+
+	@FXML
+	private ImageView imgDefaultFuncionarios;
+
+	@FXML
+	private ImageView imgDefaultLocacao;
+
+	@FXML
+	private ImageView imgDefaultProdutos;
+
+	@FXML
+	private ImageView imgDefaultUsuarios;
+
+	@FXML
+	private ImageView imgFotoPerfil;
+
+	@FXML
+	private ImageView imgLogo;
+
+	@FXML
+	private ImageView imgSair;
+
+	@FXML
+	private Button infoButton;
+
+	@FXML
+	private Label lblAtividades;
+
+	@FXML
+	private Label lblCNPJ;
+
+	@FXML
+	private Label lblFuncao;
+
+	@FXML
+	private Label lblIdEndereco;
+
+	@FXML
+	private Label lblNome;
+
+	@FXML
+	private Label lblNomeVendedor;
+
+	@FXML
+	private Label lblTelefone;
+
+	@FXML
+	private Pane pnlFormEdicao;
+
+	@FXML
+	private TextField txtAtividades;
+
+	@FXML
+	private TextField txtCNPJ;
+
+	@FXML
+	private TextField txtNome;
+
+	@FXML
+	private TextField txtTelefone;
+
+	@FXML
+	void abrirDashboard(ActionEvent event) {
+
+	}
+
+	@FXML
+	void abrirListaFornecedores(ActionEvent event) {
+
+	}
+
+	@FXML
+	void abrirListaFuncionarios(ActionEvent event) {
+
+	}
+
+	@FXML
+	void alterarFornecedor(ActionEvent event) {
+
+	}
+
+	@FXML
+	void cancelarAlteracaoFornecedor(ActionEvent event) {
+
+	}
+
+}
