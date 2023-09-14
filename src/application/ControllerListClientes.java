40d3f829c9c34343d26d1cc0ca7210c364a831cc
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-09-14 12:07:05 -0300

    Editando os métodos dentro do initializable
    Ainda falta finalizar os métodos

--- src/application/ControllerListClientes.java
+++ src/application/ControllerListClientes.java
@@ -3,18 +3,23 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import controle.LocadorDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Callback;
+import modelo.Locador;
 
 public class ControllerListClientes implements Initializable{
 
@@ -140,8 +145,45 @@ public class ControllerListClientes implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
-	}
+		LocadorDAO dao = new LocadorDAO();
+		tableClientes.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		
+		/*Tabelas
+		columnCPF
+		columnTelefone.setCellFactory(cellData);
+		columnPaisResidencia
+		columnCNH
+		columnValidadeCarteira
+		
+		*/
+		
+		//Método com os botões e o header
+		columnAcoes.setCellFactory(new Callback<TableColumn<Locador, String>, TableCell<Locador, String>>(){	
 
-}
+			/*
+			@Override
+			public TableCell<Locador, String> call(TableColumn<Locador, String> param) {
+				
+				//Declarando variáveis				
+				 private final Button viewButton = new Button();
+				 private final Button editButton= new Button();
+				
+				private final HBox buttonCountainer = new HBox(viewButton, editButton);
+				
+				{
+					buttonCountainer.setSpacing(10);
+					
+					ImageView viewImage = new ImageView();
+					
+					
+				}
+				
+				return null;
+			}
+			
+		});
+		
+	}*/
+
+		}
