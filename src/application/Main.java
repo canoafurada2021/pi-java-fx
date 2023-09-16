f9f8737dd5acbd131258f19958b2a5db54d194b1
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-09-16 14:23:54 -0300

    botoes de alter e excluir ListFuncionario e daoFuncionario
    *Botão de editar aparecendo porem n funcionando
    *Botao de excluir funcionando
    *Metodo excluir daoFuncionario
    *Metodo alter daoFuncionario

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,11 +14,11 @@ public class Main extends Application {
     	primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/imgs/favicon.png")));
 
     	
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/visao/Login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/visao/Fornecedores.fxml"));
         Parent root = loader.load();
 		root.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-		ControllerLogin controller = loader.getController(); // Obtém uma referência ao controlador
+		ControllerListFornecedores controller = loader.getController(); // Obtém uma referência ao controlador
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
