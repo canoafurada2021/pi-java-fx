8ade1837101d388646b9060350f438129a402eb5
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-10-09 14:38:10 -0300

    lsitagem de produtos - ainda c uns erros nessa bombaaaaaaaa

--- src/application/ControllerEdicaoProduto.java
+++ src/application/ControllerEdicaoProduto.java
@@ -10,14 +10,19 @@ import controle.VeiculoDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import modelo.Categoria;
 import modelo.Fornecedor;
 import modelo.Veiculo;
+import utilities.ExibePopUpErro;
+import utilities.ExibePopUpSucesso;
 
 public class ControllerEdicaoProduto implements Initializable {
 
@@ -112,7 +117,7 @@ public class ControllerEdicaoProduto implements Initializable {
 	private TextField txtTipoCambio;
 
 	@FXML
-	private TextField txtUniadeEmEstoque;
+	private TextField txtUnidadeEmEstoque;
 
 	private VeiculoDAO dao = new VeiculoDAO();
 	private ArrayList<Veiculo> veiculos = dao.listar();
@@ -126,14 +131,77 @@ public class ControllerEdicaoProduto implements Initializable {
 
 	@FXML
 	void cancelarAlteracaoProduto(ActionEvent event) {
+				Node source = (Node) event.getSource();
 
+				Stage stage = (Stage) source.getScene().getWindow();
+
+				stage.close();
 	}
 
 	
 
 	@FXML
 	void salvarAlteracaoProduto(ActionEvent event) {
+			//método de alterar
+		
+		Integer veiculosId = Integer.valueOf(txtIdVeiculo.getText());
+		Integer quantAssento = Integer.valueOf(txtQuantAssentos.getText());
+		String corVeiculo = txtCor.getText();
+		Integer espacoPortaMalas = Integer.valueOf(txtEspacoPortaMalas.getText());
+		String marca = txtMarca.getText();
+		String nome = txtNome.getText();
+		Integer notaAvaliacao = Integer.valueOf(txtNotaAvaliacao.getText());
+		Integer ano = Integer.valueOf(txtAno.getText());
+		Integer quantPortas = Integer.valueOf(txtQuantPortas.getText());
+		String cambio = txtTipoCambio.getText();
+		Long precoDia = Long.parseLong(txtPrecoPorDia.getText());
+		Integer unidadeEstoque = Integer.valueOf(txtUnidadeEmEstoque.getText());
+		
+		// Obtém o CNPJ selecionado do comboFornecedor
+		Long cnpjSelecionado = comboFornecedor.getValue();
+		// Encontra o objeto Fornecedor correspondente ao CNPJ
+		Fornecedor fornecedorSelecionado = encontrarFornecedorPorCnpj(cnpjSelecionado);
+		//idem categoriaID
+		
+		Long idSelecionado = comboCategoria.getValue();
+		Categoria categoriaSelecionada = encontrarCategoria(idSelecionado);
+		
+		
+		Veiculo veiculo = new Veiculo();
+	
+		veiculo.setAno(ano);
+		veiculo.setCor(corVeiculo);
+		veiculo.setEspaco_porta_malas(espacoPortaMalas);
+		veiculo.setId_veiculo(veiculosId);
+		veiculo.setMarca(marca);
+		veiculo.setNome(nome);
+		veiculo.setNota_avaliacao(notaAvaliacao);
+		veiculo.setPreco_por_dia(precoDia);
+		veiculo.setQuant_assento(quantAssento);
+		veiculo.setUnidade_em_estoque(unidadeEstoque);
+		veiculo.setQuant_portas(quantPortas);
+		veiculo.setTipo_cambio(cambio);
+		
+		// Define o Fornecedor no veiculo
+		veiculo.setCnpj(fornecedorSelecionado);
+		veiculo.setIdCategoria(categoriaSelecionada);
 
+		if(dao.atualizar(veiculo)) {
+			ExibePopUpSucesso.ExibirPopUpSucesso();
+			
+			Node source = (Node) event.getSource();
+			Stage stage = (Stage) source.getScene().getWindow();
+
+			stage.getOnCloseRequest().handle(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+
+			System.out.println("deu bomba " + stage);
+
+			stage.close();
+		}else {
+			ExibePopUpErro.ExibirPopUpErro();
+			System.out.println("nao atualizou");
+		}
+			
 		
 	}
 
@@ -149,6 +217,7 @@ public class ControllerEdicaoProduto implements Initializable {
 		txtAno.setText(String.valueOf(v.getAno()));
 		txtQuantPortas.setText(String.valueOf(v.getQuant_portas()));
 		txtTipoCambio.setText(v.getTipo_cambio());
+		txtUnidadeEmEstoque.setText(String.valueOf(v.getUnidade_em_estoque()));
 		
 		if (v.getIdCategoria() != null) {
 			 long categoriaId = v.getIdCategoria().getIdCategoria();  // Supondo que getIdCategoria() retorna um long
@@ -222,4 +291,21 @@ public class ControllerEdicaoProduto implements Initializable {
 			comboFornecedor.getItems().add(fornecedorInfo);
 		}
 	}
+	private Fornecedor encontrarFornecedorPorCnpj(long cnpjFornecedor) {
+	    for (Fornecedor fornecedor : fornecedores) {
+	        if (fornecedor.getCnpj() == cnpjFornecedor) {
+	            return fornecedor;
+	        }
+	    }
+	    return null;  // Retorna null se nenhum fornecedor for encontrado com o CNPJ
+	}
+	
+	private Categoria encontrarCategoria (long idDaCategoria) {
+		for (Categoria categoria : categorias) {
+			if(categoria.getIdCategoria() == idDaCategoria) {
+				return categoria;
+			}
+		}return null;
+	}
+
 }
--- src/application/ControllerListProdutos.java
+++ src/application/ControllerListProdutos.java
@@ -2,11 +2,16 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import controle.VeiculoDAO;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -26,6 +31,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 import modelo.Veiculo;
 import modelo.Vendedor;
@@ -142,6 +148,23 @@ public class ControllerListProdutos implements Initializable {
 
 	@FXML
 	private TextField txtPesquisa;
+	
+	private ObservableList<Veiculo>obsVeiculos;
+	
+	VeiculoDAO daoFor = new VeiculoDAO();
+	
+	public void tblViewVeiculoSearch() {
+		tableProdutos.getItems().clear();
+		columnIdProduto.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getId_veiculo()));
+		columnAno.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getAno()));
+		columnMarca.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getMarca()));
+		columnNome.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNome()));
+		columnPreco.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getPreco_por_dia()));
+		columnUnidade.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getUnidade_em_estoque()));
+		ObservableList<Veiculo> obsVeiculos = FXCollections.observableArrayList(daoFor.listar());
+		tableProdutos.setItems(obsVeiculos);
+	;
+	}
 
 	@FXML
 	void abrirDashboard(ActionEvent event) {
@@ -254,50 +277,96 @@ public class ControllerListProdutos implements Initializable {
 		columnAno.setCellValueFactory(cellData -> new SimpleObjectProperty(cellData.getValue().getAno()));
 		columnPreco.setCellValueFactory(cellData -> new SimpleObjectProperty(cellData.getValue().getPreco_por_dia()));
 		columnUnidade.setCellValueFactory(cellData -> new SimpleObjectProperty(cellData.getValue().getUnidade_em_estoque()));
-//		columnAcoes.setCellFactory(new Callback<TableColumn<Veiculo, String>, TableCell<Veiculo, String>>(){
-//			@Override
-//			public TableCell<Veiculo, String> call(TableColumn<Veiculo, String> param) {
-//				return new TableCell<>() {
-//					
-//					//declaracao variaveis dos botoes
-//					private final Button viewButton = new Button();
-//					private final Button editButton = new Button();
-//					
-//					private final HBox buttonContainer = new HBox (viewButton, editButton);
-//					{
-//						buttonContainer.setSpacing(10);
-//						ImageView viewImage = new ImageView(
-//								new Image(getClass().getResourceAsStream("/imgs/editar.png")));
-//						
-//						viewImage.setFitHeight(16);
-//						viewImage.setFitWidth(16);
-//						viewButton.setStyle("-fx-background-color:  #001C52; -fx-text-fill: white;");
-//						viewButton.setGraphic(viewImage);
-//						viewButton.setOnAction(event -> {
-//							Veiculo veiculo = getTableView().getItems().get(getIndex());
-//							String idVeic = veiculo.getId_veiculo();
-//							try {
-//								FXMLLoader loader = new FXMLLoader(
-//										getClass().getResource("/visao/Edicao_produto.fxml"));
-//								Parent root = loader.load();
-//								ControllerEdicaoProduto controllerNovaTela = loader.getController();
-//
-//								controllerNovaTela.setVendedor(vendedor);
-//
-//								Scene scene = new Scene(root);
-//								Stage stage = new Stage();
-//								stage.setScene(scene);
-//									
-//							
-//							}
-//							
-//						})
-//					}
-//					
-//					
-//				}
-//			}
-//		});
+		columnAcoes.setCellFactory(new Callback<TableColumn<Veiculo, String>, TableCell<Veiculo, String>>(){
+			@Override
+			public TableCell<Veiculo, String> call(TableColumn<Veiculo, String> param) {
+				return new TableCell<>() {
+					
+					//declaracao variaveis dos botoes
+					private final Button viewButton = new Button();
+					private final Button editButton = new Button();
+					
+					private final HBox buttonContainer = new HBox (viewButton, editButton);
+					{
+						buttonContainer.setSpacing(10);
+						ImageView viewImage = new ImageView(
+								new Image(getClass().getResourceAsStream("/imgs/editar.png")));
+						
+						viewImage.setFitHeight(16);
+						viewImage.setFitWidth(16);
+						viewButton.setStyle("-fx-background-color:  #001C52; -fx-text-fill: white;");
+						viewButton.setGraphic(viewImage);
+						viewButton.setOnAction(event -> {
+							Veiculo veiculo = getTableView().getItems().get(getIndex());
+							int idVeic = veiculo.getId_veiculo();
+							try {
+								FXMLLoader loader = new FXMLLoader(
+										getClass().getResource("/visao/Edicao_produto.fxml"));
+								Parent root = loader.load();
+								ControllerEdicaoProduto controllerNovaTela = loader.getController();
+
+								controllerNovaTela.seVeiculo(veiculo);
+
+								Scene scene = new Scene(root);
+								Stage stage = new Stage();
+								stage.setScene(scene);
+								
+								stage.setOnCloseRequest((EventHandler<WindowEvent>) new EventHandler<WindowEvent>() {
+									public void handle(WindowEvent we) {
+										tblViewVeiculoSearch();// Esse método eu populo o tableView (ver acima)
+									}
+								});
+								stage.show();
+							}catch (IOException e) {
+								e.printStackTrace();
+							}
+							System.out.println("edicao com id");
+							System.out.println("botao edicao clicado");
+						});
+						
+						ImageView editImage = new ImageView(
+								new Image(getClass().getResourceAsStream("/imgs/excluir.png")));
+						editImage.setFitHeight(16);
+						editImage.setFitWidth(16);
+						editButton.setGraphic(editImage);
+						editButton.setStyle("-fx-background-color: red;");
+						editButton.setOnAction(event -> {
+							Veiculo veiculo = getTableView().getItems().get(getIndex());
+							 int idVeiculo = veiculo.getId_veiculo();
+							if(daoFor.excluir(idVeiculo)) {
+								daoFor.excluir(idVeiculo);
+								getTableView().getItems().remove(veiculo);
+								System.out.println("veiculo excluidp");
+							}else {
+								System.out.println("erro ao exlucir o veiculo");
+							}
+							System.out.println("botao de excluir clicado");
+						});
+
+					}
+					@Override
+					protected void updateItem(String item, boolean empty) {
+						super.updateItem(item, empty);
+						if(empty) {
+							setGraphic(null);
+						}else {
+							setGraphic(buttonContainer);
+						}
+					}
+					};
+	            }
+	        });
+		carregarVeiculos();
+	    } 
+
+	    public void carregarVeiculos() {
+	    	VeiculoDAO dao = new VeiculoDAO();
+	    	
+	    	ArrayList<Veiculo> veiculos = dao.listar();
+	    	obsVeiculos = FXCollections.observableArrayList(veiculos);
+	    	tableProdutos.setItems(obsVeiculos);
+	    }
 
 	}
-	}
+				
+				
